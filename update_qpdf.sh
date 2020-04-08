962bd8e7716f47b6d8c0df90a87f535c6c3c5bed
Kyle McNally <kyle@kmcnally.net>
2020-04-08 15:33:05 -0400

    qpdf 10.0.0

--- update_qpdf.sh
+++ update_qpdf.sh
@@ -1,5 +1,7 @@
-wget https://github.com/qpdf/qpdf/releases/download/release-qpdf-9.1.1/qpdf-9.1.1-x86_64.AppImage -O qpdf.AppImage
+rm -rf ./bin ./lib
+wget https://github.com/qpdf/qpdf/releases/download/release-qpdf-10.0.0/qpdf-10.0.0-x86_64.AppImage -O qpdf.AppImage
 ./qpdf.AppImage --appimage-extract
+mkdir -p ./bin ./lib
 cp -R squashfs-root/usr/bin/* ./bin/
 cp -R squashfs-root/usr/lib/* ./lib/
 rm -rf squashfs-root qpdf.AppImage
